c6f3364e81b53a0a3982ffca2e3133ca35793b3c
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-18 17:55:53 -0400

    Added inline styling to the SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,23 @@ class SearchBar extends Component {
   render() {
     return (
     <div>
-        <form>
-            <div className="ui search">
+    <form className="ui segment" style={{top: '50px'}}>
+    
+            <div className="ui search" style={{position: 'relative', left: '30%'}}>
+            <label style={{display: 'block', fontSize: '20px', paddingBottom: '10px'}}>Video Search</label>
                 <div className="ui icon input">
-                    <input className="prompt" type="text" placeholder="Search for videos..."></input>
+                    
+                    <input 
+                        className="prompt" 
+                        type="text" 
+                        placeholder="Search for videos..."
+                        style={{width: '400px'}}
+                    ></input>
                     <i className="search icon"></i>
                 </div>
                 <div class="results"></div>
             </div>
-        </form>
+            </form>
       </div>
     )
   }
