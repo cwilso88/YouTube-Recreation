cf24e72ac54ed865bcb709d93e4466e987bb91ad
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-18 17:36:16 -0400

    Created the search bar with semantic ui

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-      <div>
-        Search Bar
+    <div>
+        <form>
+            <div className="ui search">
+                <div className="ui icon input">
+                    <input className="prompt" type="text" placeholder="Search for videos..."></input>
+                    <i className="search icon"></i>
+                </div>
+                <div class="results"></div>
+            </div>
+        </form>
       </div>
     )
   }
