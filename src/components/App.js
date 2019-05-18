16285b4851ebd76ed3c4a4161c73d25f0865a54c
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-18 17:16:44 -0400

    Switched App.js from functional component to a Class based component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const App = () => {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
 }
 
 export default App;
