1f63dcc189d3500fac6aae42dcf6ca4ca9a897ca
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-19 09:51:16 -0400

    Added more customization to VideoDetail.js

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const VideoDetail = ({ video }) => {
-  return (
-    <div>
-      {video.snippet.title}
-    </div>
-  )
+      if(!video) {
+        return(
+            <div className="ui segment">
+                <div className="ui active inverted dimmer">
+                    <div className="ui text loader">Checking for videos...</div>
+                </div>
+            </div>
+        ) 
+      } else {
+      return(
+        <div>
+        {video.snippet.title}
+        </div>
+      )
+    }
 }
 
 export default VideoDetail
